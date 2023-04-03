36dfeee44f9f3a2e30750595a5e36c3fabaa1e20
Anil-Gaihre <35698558+Anil-Gaihre@users.noreply.github.com>
2023-04-03 19:16:00 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 make
-./graph_loader.bin ../tuple_text_to_binary_csr_mem/toy_graph/toy.dat_beg_pos.bin ../tuple_text_to_binary_csr_mem/toy_graph/toy.dat_csr.bin ../tuple_text_to_binary_csr_mem/toy_graph/toy.dat_weight.bin
+./graph_loader.bin <path_to_row_offset/begin_pos> <path_to_adjacency_list/csr> <path_to_adjacency_weight/weight>
